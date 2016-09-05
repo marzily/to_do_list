a76dc1aa0c36d5a07777db4b76bcb882c6824d8f
margie <ly.margarett@gmail.com>
2016-09-05 11:20:46 -0700

    update list index to prepopulate checkboxes for "completed"

--- spec/features/add_todo_spec.rb
+++ spec/features/add_todo_spec.rb
@@ -1,36 +1,40 @@
 require "rails_helper"
 
-RSpec.feature "add todo to list", type: :feature do
+RSpec.feature "add ToDo to list", type: :feature do
   it 'has the right content on the new page' do
     visit new_todo_path
-    
+
     expect(page).to have_content("Create a New ToDo")
   end
 
-  it 'lets you add a Todo and redirects to show' do
+  it 'lets you add a ToDo and redirects to show' do
     visit new_todo_path
 
     fill_in("Task", with: "bake stuff")
     fill_in("Deadline", with: "09-30-2016")
-    fill_in("Completed", with: false)
     click_button("Add to List")
 
     expect(current_path).to eq(todo_path(id: Todo.last.id))
     expect(page).to have_content("bake stuff")
   end
 
-  it 'saves the Todo to the list' do
+  it 'saves the ToDo to the list' do
     visit new_todo_path
 
     fill_in("Task", with: "complete rails app")
     fill_in("Deadline", with: "09-30-2016")
-    fill_in("Completed", with: false)
+    within("form") do
+      check("todo[completed]")
+    end
     click_button("Add to List")
 
     visit todos_path
 
     within("table") do
       expect(page).to have_content("complete rails app")
+    end
+    within "table" do
+      expect(page).to have_checked_field("todo")
     end
   end
 end
