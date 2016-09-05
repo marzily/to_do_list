c3659855125ee72dff355473390f3eac0e96fe5e
margie <ly.margarett@gmail.com>
2016-09-04 21:06:59 -0700

    new todo is added, can view in show and list index

--- spec/features/add_todo_spec.rb
+++ spec/features/add_todo_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "add todo to list", type: :feature do
     expect(page).to have_content("Create a New ToDo")
   end
 
-  it 'lets you add a Todo to the list' do
+  it 'lets you add a Todo' do
     visit new_todo_path
 
     fill_in("Task", with: "bake stuff")
@@ -14,14 +14,22 @@ RSpec.feature "add todo to list", type: :feature do
     fill_in("Completed", with: false)
     click_button("Add to List")
 
-    # expect(current_path).to eq(todo_path(id: Todo.last.id))
-    # within("table") do
-      # expect(page).to have_content("bake stuff")
-    # end
+    expect(current_path).to eq(todo_path(id: Todo.last.id))
+    expect(page).to have_content("bake stuff")
+  end
+
+  it 'saves the Todo to the list' do
+    visit new_todo_path
+
+    fill_in("Task", with: "complete rails app")
+    fill_in("Deadline", with: "09-30-2016")
+    fill_in("Completed", with: false)
+    click_button("Add to List")
 
     visit todos_path
+
     within("table") do
-      expect(page).to have_content("bake stuff")
+      expect(page).to have_content("complete rails app")
     end
   end
 end
