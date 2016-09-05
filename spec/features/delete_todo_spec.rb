13c6dd6fe697177093d889cd10db860863c9b5f9
margie <ly.margarett@gmail.com>
2016-09-05 14:21:28 -0700

    add delete button from task show page

--- spec/features/delete_todo_spec.rb
+++ spec/features/delete_todo_spec.rb
@@ -8,9 +8,29 @@ RSpec.feature 'delete ToDos', type: :feature do
   }
 
   it 'has delete buttons beside ToDos in the list' do
-    visit(todos_path)
+    visit todos_path
 
     expect(page).to have_button('Delete')
   end
 
+  it 'deletes a task if "Delete" is clicked from list overview' do
+    visit todos_path
+    click_button('Delete')
+
+    expect(page).to_not have_content('make pancakes')
+  end
+
+  it 'has a delete button in the task show page' do
+    visit todo_path(id: Todo.last.id)
+
+    expect(page).to have_button('Delete')
+  end
+
+  it 'deletes a task if "Deleted" is clicked from task show page' do
+    visit todo_path(id: Todo.last.id)
+    click_button('Delete')
+
+    expect(current_path).to eq(todos_path)
+    expect(Todo.last).to eq(nil)
+  end
 end
